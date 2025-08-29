b87d1646e959a1d6348f108cad89ed81e505d578
ericsuescun <edsuescun@gmail.com>
2025-08-29 17:05:19 -0500

    Adds pundit gem. Changes to Vitalia

---
+++ app/policies/study_policy.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class StudyPolicy < ApplicationPolicy
+  attr_reader :user, :record
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(published: true)
+      end
+    end
+  end
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NoMethodError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+end
